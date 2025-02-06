b1f1e0303d72a4e0b1309842e0be922d8136eb21
tatsuya katayama <tat.katayama@ntt.com>
2025-02-07 06:41:53 +0900

    change gitignore

--- function_app.py
+++ function_app.py
@@ -4,7 +4,7 @@ import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.route(route="http_trigger")
+@app.route(route="http_trigger", auth_level=func.AuthLevel.ANONYMOUS)
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
