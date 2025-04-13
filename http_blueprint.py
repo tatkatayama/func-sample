735a3c1de1cab7a907e77c1eb43d73e1e7cc83fa
tatkatayama <tat.katayama@ntt.com>
2025-04-14 06:40:29 +0900

    関数追加

--- http_blueprint.py
+++ http_blueprint.py
@@ -5,6 +5,7 @@ import azure.functions as func
 bp = func.Blueprint() 
 
 @bp.route(route="default_template") 
+@bp.route(route="sample1")
 def default_template(req: func.HttpRequest) -> func.HttpResponse: 
     logging.info('Python HTTP trigger function processed a request.') 
 
@@ -28,3 +29,11 @@ def default_template(req: func.HttpRequest) -> func.HttpResponse:
             " personalized response.", 
             status_code=200 
         )
+def sample1(req: func.HttpRequest) -> func.HttpResponse: 
+    logging.info('Python HTTP trigger function processed a request.') 
+    return func.HttpResponse( 
+        "This HTTP-triggered function executed successfully. " 
+        "Pass a name in the query string or in the request body for a" 
+        " personalized response.", 
+        status_code=200 
+    )
